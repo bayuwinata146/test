c52120e2b385a943de786ff36ccd46089a0132d9
bayuwinata146 <bayuwinata146@gmail.com>
2025-10-16 03:10:36 +0000

    feat: install tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test</title>
-</head>
-<body>
-    <h1>hello world</h1>
-</body>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold font-red-600">
+      Hello world! 
+    </h1>
+  </body>
 </html>
